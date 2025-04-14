7a09dcd08fd1820f5836579582d6c81705ad69df
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-04-14 14:13:45 +0200

    Eerste opzet
    Doorlopen van de stappen
    
    #19

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-console.log('Hier komt je server voor Sprint 10.')
+import express, { urlencoded } from 'express';
+import { Liquid } from 'liquidjs';
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+const app = express();
+app.use(express.static('public'));
 
-console.log('Zet \'m op!')
+const engine = new Liquid();
+app.engine('liquid', engine.express()); 
+
+app.get('/', (req, res) => {
+res.render('index.liquid')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact.liquid')
+})
+
+app.get('/publications', (req, res) => {
+    res.render('publications.liquid')
+})
+
+app.set('port',process.env.PORT || 8000)
+
+app.listen(app.get('port'),() => {
+
+})
