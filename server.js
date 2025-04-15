3d2a20af17d66d706e433c2e03ca9b737558b93f
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-04-15 09:41:13 +0200

    Header opgezet
    Eerst opzet van de header (met select option gebruikt)
    
    #1

--- server.js
+++ server.js
@@ -11,12 +11,29 @@ app.get('/', (req, res) => {
 res.render('index.liquid')
 })
 
+app.get('/over-ons', (req, res) => {
+    res.render('index.liquid')
+    })
+
 app.get('/contact', (req, res) => {
     res.render('contact.liquid')
 })
 
-app.get('/publications', (req, res) => {
+app.get('/publicaties', (req, res) => {
+    // maak een per publicatie die uit de database komt
     res.render('publications.liquid')
+})
+
+app.get('/publciaties/:slug', (req, res) => {
+    res.render('publications')
+})
+
+app.get('/leden', (req, res) => {
+    res.render('contact.liquid')
+})
+
+app.get('/vacatures', (req, res) => {
+    res.render('contact.liquid')
 })
 
 app.set('port',process.env.PORT || 8000)
